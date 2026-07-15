4356bb99ef1bfdcfbb7ccd769a24b48555bc6aa5
rhys wang <xuehaq@gmail.com>
2026-07-15 17:24:14 +0800

    ☕ Initial Project Source Code

---
+++ tests/MarineInsight.Domain.Tests/TestAssemblySmokeTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Versioning;
+
+namespace MarineInsight.Domain.Tests;
+
+public sealed class TestAssemblySmokeTests
+{
+    [Fact]
+    public void TestAssemblyTargetsDotNetTen()
+    {
+        var targetFramework = typeof(TestAssemblySmokeTests)
+            .Assembly
+            .GetCustomAttributes(inherit: false)
+            .OfType<TargetFrameworkAttribute>()
+            .Single()
+            .FrameworkName;
+
+        Assert.Equal(".NETCoreApp,Version=v10.0", targetFramework);
+    }
+}
